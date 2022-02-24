af42df81c67e1134fc01fb46c9e02aeccd8a5844
JuanLP <juan.luis.palacios.p@gmail.com>
2022-02-24 06:27:55 -0500

    Add tests for the new functionality

--- functions.test.js
+++ functions.test.js
@@ -13,5 +13,13 @@ describe('stringLength(string)', ()=>{
     expect(stringLength('aaa')).toBe(3);
     expect(stringLength('aaaaa')).toBe(5);
   });
+  it ('should reject strings shorter than 1 character', ()=>{
+    expect(()=>stringLength('')).toThrow(Error);
+  });
+  it ('should reject strings longer than 10 characters', ()=>{
+    expect(()=>stringLength('dsfgsdfgsdfgsdfgsdfgsdg')).toThrow(Error);
+    expect(()=>stringLength('this is too long')).toThrow(Error);
+    expect(()=>stringLength('just right')).not.toThrow(Error);
+  });
 });
 
