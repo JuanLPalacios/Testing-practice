444f9565581a701477a2560cf91f741deaa35156
JuanLP <juan.luis.palacios.p@gmail.com>
2022-02-24 06:23:02 -0500

    Expand stringLength to test for length

--- functions.js
+++ functions.js
@@ -1,5 +1,9 @@
 export function stringLength(string) {
     if (typeof string !== 'string')
         throw new Error('first parameter should be a a string');
+    if (string.length==0)
+        throw new Error('string cannot be shorter than 1 character');
+    if (string.length>10)
+        throw new Error('string cannot be longer than 10 characters');
     return string.length;
 }
